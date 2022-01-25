39f2c53d64f3a5cda346d47a1e5e22b34e16b7ff
Carlos Pasquier <64280091+capj1@users.noreply.github.com>
2022-01-24 21:45:12 -0600

    Bootstrap function and plot

---
+++ R/plotrwa_boot.R
@@ -0,0 +1,22 @@
+# Plot for bootmodelrwa objects
+#'
+#' @param x an object of class "modelrwa"
+#' @param title an optional title for the plot
+#'
+#' @return Returns a ggplot object
+#' @export
+#'
+#' @examples
+#'
+
+
+
+
+plotbootrwa <- function(x,title=""){
+  p2 <- ggplot2::ggplot(x) + geom_boxplot(ggplot2::aes(Variable,Weight))+ggplot2::theme_minimal(base_size = 12) +
+    ggplot2::scale_y_continuous(labels = scales::percent) +
+    ggplot2::ggtitle(title)
+
+  return(p2)
+
+}
