7a07d890227cab8484cbe6e7a0e74d38fda94046
Maikol Solís <maikol.solis@ucr.ac.cr>
2022-06-28 05:33:40 -0600

    fix: add namespace ggplot2::geom_boxplot

--- R/plotrwa_boot.R
+++ R/plotrwa_boot.R
@@ -17,7 +17,7 @@ plotbootrwa <- function(x,title=""){
   x$Variable <- reorder(x$Variable, x$Weight)
   p2 <-
     ggplot2::ggplot(x) +
-    geom_boxplot(ggplot2::aes(Variable, Weight)) +
+    ggplot2::geom_boxplot(ggplot2::aes(Variable, Weight)) +
     ggplot2::theme_minimal(base_size = 12) +
     ggplot2::scale_y_continuous(labels = scales::percent) +
     ggplot2::ggtitle(title) +
