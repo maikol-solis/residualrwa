0439f2619012df3b8273dfbb39c981de8a1b7918
Maikol Solís <maikol.solis@ucr.ac.cr>
2022-06-29 06:34:24 -0600

    refactor!(plot): plot using the new format
    BREAKING CHANGE: the commit adds the bootstraping plots into the main
    function.
    
    - If `boot_ci=TRUE` an histogram with the estimated weight is plot for
    each variable.
    - If `boot_ci=FALSE` a lollipop graph is used to visualize the single
    weight.

--- R/plot_rwa.R
+++ R/plot_rwa.R
@@ -10,17 +10,37 @@
 #' @examples
 plot_rwa <- function(x, title = "") {
   df <- x$data_frame
-  variable <- weight <- NULL
-
   df$variable <- reorder(df$variable, df$weight)
 
-  p2 <- ggplot2::ggplot(data = df) +
-    ggplot2::geom_bar(ggplot2::aes(variable, weight), stat = "identity") +
-    ggplot2::theme_minimal(base_size = 12) +
-    ggplot2::scale_y_continuous(labels = scales::percent) +
-    ggplot2::ggtitle(title) +
-    ggplot2::coord_flip()
-
-
-  return(p2)
+  if (x$boot_ci == TRUE) {
+    df_boot <- x$data_frame_boot
+    df_boot$variable <- reorder(df_boot$variable, df_boot$weight)
+    ggplot2::ggplot(data = df_boot) +
+      ggplot2::geom_boxplot(ggplot2::aes(x = variable, y = weight)) +
+      ggplot2::geom_point(
+        data = df,
+        ggplot2::aes(x = variable, y = weight),
+        color = "red", fill = "red", size = 2, shape = 23
+      ) +
+      ggplot2::theme_minimal(base_size = 12) +
+      ggplot2::scale_y_continuous(labels = scales::percent) +
+      ggplot2::ggtitle(title) +
+      ggplot2::coord_flip()
+  } else {
+    ggplot2::ggplot(data = df) +
+      ggplot2::geom_segment(ggplot2::aes(
+        x = variable,
+        xend = variable,
+        y = 0,
+        yend = weight
+      )) +
+      ggplot2::geom_point(
+        ggplot2::aes(x = variable, y = weight),
+        color = "red", fill = "red", size = 2, shape = 23
+      ) +
+      ggplot2::theme_minimal(base_size = 12) +
+      ggplot2::scale_y_continuous(labels = scales::percent) +
+      ggplot2::ggtitle(title) +
+      ggplot2::coord_flip()
+  }
 }
